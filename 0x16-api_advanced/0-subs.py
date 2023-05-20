0884b01c6032c21feb7064a34edcb81c852d6be4
Jackline Muraya <jacklinewanja.jw@gmail.com>
2023-05-20 14:51:32 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
        return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0)" \
 
     }
     response = get(url, headers=headers).json()
